1db54863a5b72525ddd2ff300b9edbb7e7680fba
olusolabobby <olusolaadelabu89@gmail.com>
2023-09-26 17:54:42 +0300

    home

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,14 @@
-import React from "react";
+import React, {useRef} from "react";
 import styles from "./Home.module.css";
+import ScrollDivider from "../ScrollDivider";
 
 const Home = () => {
+
+    // const ref = useRef(null);
+    // const handleClick = () => {
+    //     ref.current?.scrollIntoView({ behavior: "smooth"});
+    // }
+
   return (
       <div className={styles.homeContainer}>
           <div className={styles.firstHomeContainer}>
@@ -33,11 +40,11 @@ const Home = () => {
                                 <div className={styles.buttonText2}>Generates additional revenue</div>
                             </div>
                             <div className={styles.downClickImg}>
-                                <img src="/images/downClick.png" alt=''/>
+                                <img src="/images/downClick.png" alt=''
+                                // onClick={handleClick}
+                                />
                             </div>
                         </div>
-
-
                         <div className={styles.leftMain}>
                             <div className={styles.leftMainImg}>
                                 <img className={styles.imgInfo} src="images/iPhone_14 _Pro.png" alt=""/>
@@ -47,6 +54,8 @@ const Home = () => {
                   </div>
               </div>
 
+              {/*<ScrollDivider ref={ref} className={styles.scrollDivider } />*/}
+
               <div className={styles.belowFirst}>
                 <div className={styles.innerBelowFirst1}>
                     <div className={styles.innerBelowBox1}>
@@ -55,6 +64,7 @@ const Home = () => {
                         <div className={styles.study}>A study by App Annie found that adding Offerwalls to your existing monetization strategy increases retention by 50% - Why? The answer is…more choice for the user. </div>
                     </div>
                 </div>
+
                 <div className={styles.innerBelowFirst2}>
                     <div className={styles.innerBelowBox2}>
                         <div className={styles.blog}>Blog</div>
@@ -67,8 +77,8 @@ const Home = () => {
                     </div>
                 </div>
               </div>
-
           </div>
+
 
           <div className={styles.secondHomeContainer}>
                 <div className= {styles.secondHomeContainerBox1}>
@@ -76,6 +86,7 @@ const Home = () => {
                         <div className={styles.what}>What Is an </div>
                         <div className={styles.offerW}>Offerwall?</div>
                     </div>
+
                     <div className={styles.anOfferwall}>
                         <div className={styles.innerAnOfferwall}>
                             <div className={styles.anOfferwallParag}>
@@ -92,6 +103,99 @@ const Home = () => {
                 <div className= {styles.secondHomeContainerBox2}>
                     <img src="/images/mobileP.png" alt=""/>
                 </div>
+          </div>
+
+
+          <div className={styles.thirdHomeContainer}>
+              <div style={{ backgroundImage: "url(/images/Th1.png)", height: "750px"}}>
+                <div className={styles.thirdHomeContainerBox}>
+                    <div className={styles.mobileProblem}>
+                        <div className={styles.mobileProblem1}>The Problem</div>
+                        <div className={styles.mobileProblem2}> With Modern Mobile Game Monetization</div>
+                    </div>
+
+                    <div className={styles.gameDeveloper}>
+                        <div className={styles.gameDeveloper1}>Many game developers struggle with understanding
+                        the best way to monetize a mobile game, resorting
+                        to flooding their users with ads that interrupt the
+                            user experience and cause their users to write negative reviews.
+                        </div>
+                    </div>
+
+                </div>
+              </div>
+
+          </div>
+
+
+          <div className={styles.fourthHomeContainer}>
+                <div className={styles.fourthHomeBox1}>
+                    <div>Why</div>&nbsp;
+                    <div className={styles.innerFourthHomeBox1}>Mobile </div> &nbsp;
+                    <div>Offerwall Monetization? </div>
+                </div>
+
+                <div className={styles.fourthHomeBox2}>
+                    <div className={styles.fourthBox2Container}>
+                        <div className={styles.fourth13}>
+                            <div className={styles.fourth13One}>
+                                <div className={styles.innerOne}>
+                                    <img src="/images/one.png" alt="" />
+                                    <div className={styles.fourth13Integration}>
+                                        Offerwall integration is seamless with existing ad monetization or paywall structures
+                                    </div>
+                                </div>
+                            </div>
+                            <div className={styles.fourth13Two}>
+                                <div className={styles.innerOne}>
+                                    <img src="/images/three.png" alt="" />
+                                    <div className={styles.fourth13Integration}>
+                                        At the point where users decline to pay
+                                        for premium content in your online shop you
+                                        can redirect them to an offerwall giving
+                                        them a different way to make the same payment                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className={styles.fourthImg}>
+                            <img src="/images/phone01.png" alt=""/>
+                        </div>
+
+                        <div className={styles.fourth24}>
+                            <div className={styles.fourth24One}>
+                                <div className={styles.innerOne}>
+                                    <img src="/images/two.png" alt="" />
+                                    <div className={styles.fourth13Integration}>
+                                        Offerwall integration is seamless with existing ad monetization or paywall structures
+                                    </div>
+                                </div>
+                            </div>
+                            <div className={styles.fourth24Two}>
+                                <div className={styles.innerTwoSecond}>
+                                    <img src="/images/four.png" alt="" />
+                                    <div className={styles.fourth13Integration}>
+                                        At the point where users decline to pay
+                                        for premium content in your online shop you
+                                        can redirect them to an offerwall giving
+                                        them a different way to make the same payment                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div className={styles.fourthHomeBox3}>
+                    <div className={styles.innerFourthHomeBox3}>
+                        OK… But Do Offerwalls Work?
+                    </div>
+                </div>
+          </div>
+
+
+          <div className={styles.fifthHomeContainer}>
+              <div style={{ backgroundImage: "url(/images/rect01.png)", height: "750px"}}>
+               </div>
           </div>
 
 
